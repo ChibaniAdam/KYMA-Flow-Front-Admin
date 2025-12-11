9fe1ec2cd79af78cc8e6e1735226f28b0e486e73
ChibaniAdam <adam.chibani@esprit.tn>
2025-12-11 19:33:17 +0100

    implemented LDAP login ready

--- src/pages/auth/login/login.tsx
+++ src/pages/auth/login/login.tsx
@@ -55,7 +55,7 @@ const Login = () => {
               type="text"
               value={uid}
               onChange={(e) => setUid(e.target.value)}
-              className="w-full px-4 py-2 border focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="w-full px-4 py-2 border focus:outline-none focus:ring-2 focus:ring-blue-400 login-input"
               placeholder="Enter your username (e.g., john.doe)"
               required
               disabled={loading}
